35efc0e35165e454a940b18930be79409329b274
PatHealy <pwh5@pitt.edu>
2018-02-24 16:47:58 -0500

    Created director with spawn pile

---
+++ RequiemforWalking/Assets/_Scripts/Director.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Director : MonoBehaviour {
+	public Transform kyle;
+	int count;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		count++;
+		if (count > 15) {
+			count = 0;
+			Instantiate (kyle, new Vector3 (0, gameObject.GetComponent<Transform>().position.y - 5, 0), Quaternion.identity);
+		}
+	}
+}
